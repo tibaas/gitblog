0a2ed08925a56568acc899eb422ff51ced191538
tibaas <igorox2@hotmail.com>
2023-10-30 22:41:24 -0300

    initial commit

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -3,7 +3,6 @@ import { defaultTheme } from '../styles/themes/default'
 
 type ThemeType = typeof defaultTheme
 
-//extender a typagem que o styled já tem e não sobrescrever  ela por isso importar no começo
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
 }
